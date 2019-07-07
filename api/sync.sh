dd461244f7edf62e045efa08d6115d54e82ccd17
Lvsi-China <15239928381@163.com>
2019-07-07 18:23:10 +0800

    修改

--- api/sync.sh
+++ api/sync.sh
@@ -3,6 +3,9 @@ scp -r /Users/yimutian/Lvsi/Private-work/tiktok/api/app root@154.8.226.77:/home/
 
 scp -r /Users/yimutian/Lvsi/Private-work/tiktok/api/routes root@154.8.226.77:/home/wwwroot/tiktok.tiantianquan.com/api/
 
+scp -r /Users/yimutian/Lvsi/Private-work/tiktok/api/database root@154.8.226.77:/home/wwwroot/tiktok.tiantianquan.com/api/
+
+
 cd ~/Lvsi/Private-work/tiktok/api
 
 git pull origin
